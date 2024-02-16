4a7d44d60e7346780ac12f3ace4e8af5b26b94d7
Kilian Vitou <vitoukilian@gmail.com>
2024-02-16 20:04:25 +0100

    make fixes

--- qrmenu_backend/settings.py
+++ qrmenu_backend/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 
@@ -137,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
