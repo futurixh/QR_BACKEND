c245d794b0e8a164462f7ade801952bba1941e38
Kilian Vitou <vitoukilian@gmail.com>
2024-02-18 04:05:03 +0100

    debug

--- core/permissions.py
+++ core/permissions.py
@@ -3,18 +3,20 @@ from rest_framework import permissions
 from . import models
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
-  
+
   def has_object_permission(self, request, view, obj):
     # Read permissions are allowed to any request,
     # so we'll always allow GET, HEAD or OPTIONS requests.
     if request.method in permissions.SAFE_METHODS:
         return True
+    if request.user.is_anonymous:
+        isinstance.user = None
 
     # Write permissions are only allowed to the owner of the place.
     return obj.owner == request.user
 
 class PlaceOwnerOrReadOnly(permissions.BasePermission):
-  
+
   def has_object_permission(self, request, view, obj):
     # Read permissions are allowed to any request,
     # so we'll always allow GET, HEAD or OPTIONS requests.
